5891e867906d8a44f6e311270e7f4cd3d1d777b2
Quentin Nater <quentin.nater@students.hevs.ch>
2019-12-14 09:29:34 +0100

    VM

--- CoffeeCashlessWeb/ViewModels/TransactionVM.cs
+++ CoffeeCashlessWeb/ViewModels/TransactionVM.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using DTO;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,11 +7,67 @@ namespace CoffeeCashlessWeb.ViewModels
 {
     public class TransactionVM
     {
-        [Required(ErrorMessage = "Veuillez entrer votre ID !")]
-        public int IdAccount { get; set; }
-
-        public int IdProduct { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+        public int IdRoom { get; set; }
+        public int IdClient { get; set; }
         public string Name { get; set; }
-        public double Price { get; set;}
+        public string Lastname { get; set; }
+        public string Forname { get; set; }
+        public double Days { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total { get; set; }
+
+        public decimal FinalTotal { get; set; }
+
+        public string Location { get; set; }
+        public int Category { get; set; }
+        public string Pic1 { get; set; }
+        public string Pic2 { get; set; }
+        public string Pic3 { get; set; }
+        public int CliNumber { get; set; }
+        public int DateInDays { get; set; }
+        public int DateInMonths { get; set; }
+        public int DateInYears { get; set; }
+        public int DateOutDays { get; set; }
+        public int DateOutMonths { get; set; }
+        public int DateOutYears { get; set; }
+        public bool Tv { get; set; }
+        public bool Hairdryer { get; set; }
+        public bool Parking { get; set; }
+        public bool Wireless { get; set; }
+        public int Error { get; set; }
+        public int NumberRoom { get; set; }
+
+        public List<int> IdList { get; set; }
+        public List<int> IdNumber { get; set; }
+
+        public decimal TotalWithRoom { get; set; }
+
+        public TransactionVM(int idRoom, int idClient, DateTime dateIn, DateTime dateOut, string lastname, string forname, double days, decimal price, List<int> idList, List<int> idNumber, int nbPerson, decimal nbrSolo, decimal nbrDuo)
+        {
+            IdRoom = idRoom;
+            IdClient = idClient;
+            DateIn = dateIn;
+            DateOut = dateOut;
+            Lastname = lastname;
+            Forname = forname;
+            Days = days;
+
+            Price = price;
+            IdList = idList;
+            IdNumber = idNumber;
+            CliNumber = nbPerson;
+
+            Total = (decimal)(days * (int)Price); // Total Price
+            FinalTotal = Total * nbPerson; // Final Price
+
+            TotalWithRoom = (decimal)(days) * (nbrSolo + nbrDuo); // Total price for a booking
+        }
+
+      
+
+
     }
+
 }
