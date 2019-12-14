63e31f87446ade583c8e60ef28a4ffd68e833689
Nortah <nes96@hotmail.ch>
2019-12-14 09:13:12 +0100

    ScriptSQL Correction

--- ScriptsSQL/dbo.Transaction.sql
+++ ScriptsSQL/dbo.Transaction.sql
@@ -2,6 +2,9 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Date] DATETIME NOT NULL, 
-    [IdAccount] INT NOT NULL, 
-    [IdTransaction] INT NOT NULL
+    [AccountFK] INT NOT NULL, 
+    [ProductFK] INT NOT NULL,
+	FOREIGN KEY (AccountFK) REFERENCES Account(Id),
+	FOREIGN KEY (ProductFK) REFERENCES Product(Id);
+
 )
