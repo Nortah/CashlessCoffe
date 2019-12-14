c54697f4cc2dc5a8a5b075d2e983ac2174af5db2
yevhe <yevhe@LAPTOP-35UFDHVQ>
2019-12-14 11:26:26 +0100

    Controler

--- CoffeeCashlessWeb/Controllers/ConsoleController.cs
+++ CoffeeCashlessWeb/Controllers/ConsoleController.cs
@@ -3,87 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
+using DTO;
+using CoffeeCashlessWeb.ViewModels;
+using CoffeeCashlessWeb.Models;
+
 
 namespace CoffeeCashlessWeb.Controllers
 {
     public class ConsoleController : Controller
     {
-        // GET: Console
+      
         public ActionResult Index()
         {
-            return View();
+
+            ProductM pvm = new ProductM
+            {
+                Names = ProductManager.GetAllProductName()
+            };
+            return View(pvm);
         }
 
-        // GET: Console/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Console/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Console/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Index(ProductM pvm = null)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
+       
+                Session["pvm"] = pvm;
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
+                
+                Transaction transaction = new Transaction
+                {
+                    Date = DateTime.Now,
+                    AccountFK = pvm.Id,
+                    ProductFK = ProductManager.GetIdByProductName(pvm.Name)
+                };
+
+                TransactionManager.AddTransaction(transaction);
+
+                ProductM pvm1 = new ProductM
+                {
+                    Names = ProductManager.GetAllProductName()
+                };
+
+                return View(pvm1);
             }
         }
 
-        // GET: Console/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Console/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Console/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Console/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
